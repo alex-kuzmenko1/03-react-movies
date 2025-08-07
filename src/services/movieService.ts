c99c9e97204eb0d266adc9e7534ab612fd45ede3
Oleksandr Kuzmenko <alexaniyyy@gmail.com>
2025-08-07 13:58:19 +0100

    ch

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,23 +1,25 @@
-import axios from 'axios';
-import { Movie } from '../types/movie';
+import axios from "axios";
+import { Movie } from "../types/movie";
 
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = "https://api.themoviedb.org/3";
 const SEARCH_URL = `${BASE_URL}/search/movie`;
 
 interface FetchMoviesParams {
   query: string;
 }
 
-export const fetchMovies = async ({ query }: FetchMoviesParams): Promise<Movie[]> => {
+export const fetchMovies = async ({
+  query,
+}: FetchMoviesParams): Promise<Movie[]> => {
   const response = await axios.get(SEARCH_URL, {
     params: {
       query,
       include_adult: false,
-      language: 'en-US',
+      language: "en-US",
       page: 1,
     },
     headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTcwYjcwZWNkY2MzNmY2YTM0YmY5OTk1YzJiMTI4OCIsIm5iZiI6MTc0ODczMTQ5Ny45OTksInN1YiI6IjY4M2I4NjY5YjI5MGQ1ZDJjMDI4ODZiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.76CIlWte2ofDyjuy7bPcEKzwp-A7OWwQfMFil2IJs24`,
     },
   });
 
